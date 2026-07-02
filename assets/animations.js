948b50f22b37ffadea81db736540e47d8a85f9d7
divakar-rajodiya <divakarrajodiya@gmail.com>
2026-07-02 16:16:53 +0530

    fix: changes coming from the shopify pull.

--- assets/animations.js
+++ assets/animations.js
@@ -42,7 +42,8 @@ function initializeScrollAnimationTrigger(rootEl = document, isDesignModeEvent =
 function initializeScrollZoomAnimationTrigger() {
   if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) return;
 
-  const animationTriggerElements = Array.from(document.getElementsByClassName(SCROLL_ZOOM_IN_TRIGGER_CLASSNAME));
+  const animationTriggerElements = Array.from(document.getElementsByClassName(SCROLL_ZOOM_IN_TRIGGER_CLASSNAME))
+    .filter((element) => !element.closest('.slideshow-wrapper'));
 
   if (animationTriggerElements.length === 0) return;
 
